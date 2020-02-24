86e9666318ab67634e19de420bc896ea58cfa38d
Phillip Burgess <paintyourdragon@dslextreme.com>
2020-02-24 14:40:05 -0800

    Update example for Feather M4+wing
    Library still needs work. Not crashing, am getting some stuff on matrix, but incorrect & glitchy.

--- examples/protomatter/protomatter.ino
+++ examples/protomatter/protomatter.ino
@@ -20,14 +20,34 @@ PA04 A3    PA12 MISO  PA20 D9    PB04       PB12 D7    PB20
 PA05 A1    PA13 SCK   PA21 D8    PB05       PB13 D4    PB21
 PA06 A2    PA14 MISO  PA22 D1    PB06       PB14 D5    PB22
 PA07       PA15       PA23 D0    PB07       PB15 D6    PB23
+
+FEATHER M4:
+PA00       PA08       PA16 D5    PB08 A2    PB16 D1/TX
+PA01       PA09       PA17 SCK   PB09 A3    PB17 D0/RX
+PA02 A0    PA10       PA18 D6    PB10       PB18     
+PA03       PA11       PA19 D9    PB11       PB19     
+PA04 A4    PA12 SDA   PA20 D10   PB12       PB20
+PA05 A1    PA13 SCL   PA21 D11   PB13       PB21
+PA06 A5    PA14 D4    PA22 D12   PB14       PB22 MISO
+PA07       PA15       PA23 D13   PB15       PB23 MOSI
+
+RGB Matrix FeatherWing:
+R1  D6    A   A5
+G1  D5    B   A4
+B1  D9    C   A3
+R2  D11   D   A2
+G2  D10   LAT D0/RX
+B2  D12   OE  D1/TX
+CLK D13
+RGB+clock in one PORT byte on Feather M4!
 */
 
 #if defined(__SAMD51__)
-  uint8_t rgbPins[]  = {8, 9, 10, 11, 12, 13};
-  uint8_t addrPins[] = {4, 5, 6, 7};
-  uint8_t clockPin   = 1;
+  uint8_t rgbPins[]  = {6, 5, 9, 11, 10, 12};
+  uint8_t addrPins[] = {A5, A4, A3, A2};
+  uint8_t clockPin   = 13;
   uint8_t latchPin   = 0;
-  uint8_t oePin      = 2;
+  uint8_t oePin      = 1;
 #else // SAMD21
   uint8_t rgbPins[]  = {6, 7, 10, 11, 12, 13};
   uint8_t addrPins[] = {0, 1, 2, 3};
