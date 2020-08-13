d182e7f227bdb9ad6492601e862773932fe8a0b8
Doxygen CI <travis@travis-ci.org>
2020-08-13 15:14:58 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_d.js
+++ html/search/all_d.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['remap_5fg',['remap_g',['../struct_protomatter__core.html#a72512fca076260636d89f17a0428e544',1,'Protomatter_core']]],
-  ['remap_5frb',['remap_rb',['../struct_protomatter__core.html#a818b57b2732db02a54e249810c54023b',1,'Protomatter_core']]],
   ['rgbandclockmask',['rgbAndClockMask',['../struct_protomatter__core.html#afe77e0945602b662a968d1019bc107f6',1,'Protomatter_core']]],
   ['rgbmask',['rgbMask',['../struct_protomatter__core.html#a7fc666e797c64d1d307348566d4efb21',1,'Protomatter_core']]],
   ['rgbpins',['rgbPins',['../struct_protomatter__core.html#a884e952a8f8d790511a62806ff16433b',1,'Protomatter_core']]],
--- html/search/defines_0.js
+++ html/search/defines_0.js
@@ -6,7 +6,6 @@ var searchData=
   ['_5fpm_5fclockholdhigh',['_PM_clockHoldHigh',['../arch_8h.html#a486e988c1a6ab7121383a389c936e76d',1,'arch.h']]],
   ['_5fpm_5fclockholdlow',['_PM_clockHoldLow',['../arch_8h.html#aeb7d2c59d87d665cb80532e655d16ef0',1,'arch.h']]],
   ['_5fpm_5ffree',['_PM_FREE',['../arch_8h.html#ac1c38418405aa6afa179ddda55cb8744',1,'arch.h']]],
-  ['_5fpm_5fgamma',['_PM_GAMMA',['../core_8c.html#ad984ca93597d418a5e80d80276e11c48',1,'core.c']]],
   ['_5fpm_5fmax_5frefresh_5fhz',['_PM_MAX_REFRESH_HZ',['../_adafruit___protomatter_8cpp.html#ae4ad109606de0143863f6fa67bc65a7c',1,'_PM_MAX_REFRESH_HZ():&#160;Adafruit_Protomatter.cpp'],['../core_8c.html#ae4ad109606de0143863f6fa67bc65a7c',1,'_PM_MAX_REFRESH_HZ():&#160;core.c']]],
   ['_5fpm_5fminminperiod',['_PM_minMinPeriod',['../arch_8h.html#a91c8dfef7687605312a9616dd4a7fa64',1,'arch.h']]],
   ['_5fpm_5fport_5ftype',['_PM_PORT_TYPE',['../arch_8h.html#ac5bd6ee8eef7d07a9048da3c7e1f23b0',1,'arch.h']]],
