d52180b7474476df212e2c7425da899c476f37cb
Phillip Burgess <paintyourdragon@dslextreme.com>
2022-05-27 15:10:45 -0700

    Some progress on S3!

--- src/arch/esp32.h
+++ src/arch/esp32.h
@@ -19,15 +19,16 @@
 
 #if defined(ESP32)
 
+// Just FYI there's a bit of repetition in the defines/funcs for the
+// different ESP32 variants. The if-this-then-that permutations started
+// to get really complicated and it was cleaner just to break out each
+// variant and replicate those items for each.
+
 #include "driver/timer.h"
 
-#if defined(CONFIG_IDF_TARGET_ESP32C3)
-
-#define _PM_portOutRegister(pin) (volatile uint32_t *)&GPIO.out
-#define _PM_portSetRegister(pin) (volatile uint32_t *)&GPIO.out_w1ts
-#define _PM_portClearRegister(pin) (volatile uint32_t *)&GPIO.out_w1tc
-
-#else
+#if defined(CONFIG_IDF_TARGET_ESP32S2) // **********************************
+// *** S2 *** S2 *** S2 *** S2 *** S2 *** S2 *** S2 *** S2 *** S2 *** S2 ***
+//
 
 #define _PM_portOutRegister(pin)                                               \
   (volatile uint32_t *)((pin < 32) ? &GPIO.out : &GPIO.out1.val)
@@ -35,8 +36,6 @@
   (volatile uint32_t *)((pin < 32) ? &GPIO.out_w1ts : &GPIO.out1_w1ts.val)
 #define _PM_portClearRegister(pin)                                             \
   (volatile uint32_t *)((pin < 32) ? &GPIO.out_w1tc : &GPIO.out1_w1tc.val)
-
-#if defined(CONFIG_IDF_TARGET_ESP32S2)
 
 // On ESP32-S2, use the Dedicated GPIO peripheral, which allows faster bit-
 // toggling than the conventional GPIO registers. Unfortunately NOT present
@@ -98,7 +97,7 @@ static uint32_t _PM_directBitMask(Protomatter_core *core, int pin) {
   return (1U << (pin & 31));
 }
 // Thankfully, at present, any code which calls _PM_portBitMask() currently
-// has a 'core' variable.
+// has a 'core' variable, so we can do this...
 #define _PM_portBitMask(pin) _PM_directBitMask(core, pin)
 
 // Dedicated GPIO requires a complete replacement of the "blast" functions
@@ -136,14 +135,103 @@ IRAM_ATTR static void blast_byte(Protomatter_core *core, uint8_t *data) {
   // rgbAndClockMask is an 8-bit value when toggling, hence offset here.
   *gpio = 0b10101010101010; // Clear RGB + clock bits
 }
+
 // If using custom "blast" function(s), all three must be declared.
 // Unused ones can be empty, that's fine, just need to exist.
 IRAM_ATTR static void blast_word(Protomatter_core *core, uint16_t *data) {}
 IRAM_ATTR static void blast_long(Protomatter_core *core, uint32_t *data) {}
 
-#endif // end ESP32S2
+//
+// ***** END S2 *** END S2 *** END S2 *** END S2 *** END S2 *** END S2 *****
+#elif defined(CONFIG_IDF_TARGET_ESP32S3) // ********************************
+// *** S3 *** S3 *** S3 *** S3 *** S3 *** S3 *** S3 *** S3 *** S3 *** S3 ***
+//
 
-#endif // end !ESP32C3
+#define _PM_portOutRegister(pin)                                               \
+  (volatile uint32_t *)((pin < 32) ? &GPIO.out : &GPIO.out1.val)
+#define _PM_portSetRegister(pin)                                               \
+  (volatile uint32_t *)((pin < 32) ? &GPIO.out_w1ts : &GPIO.out1_w1ts.val)
+#define _PM_portClearRegister(pin)                                             \
+  (volatile uint32_t *)((pin < 32) ? &GPIO.out_w1tc : &GPIO.out1_w1tc.val)
+
+// On ESP32-S3, use the LCD_CAM peripheral for fast parallel output.
+// Thanks to ESP's pin MUXing, matrix data in RAM can ALWAYS be stored in
+// byte format regardless how RGB+clock bits are distributed among pins.
+#define _PM_bytesPerElement 1
+#define _PM_byteOffset(pin) 0
+#define _PM_wordOffset(pin) 0
+
+#include <soc/lcd_cam_reg.h>
+#include <soc/lcd_cam_struct.h>
+#include <hal/dma_types.h>
+#include <esp_private/gdma.h>
+#include <hal/gpio_hal.h>
+#include <esp_rom_gpio.h>
+#include <driver/periph_ctrl.h>
+
+// Override the behavior of _PM_portBitMask macro so instead of returning
+// a 32-bit mask for a pin within its corresponding GPIO register, it instead
+// returns a 6-bit mask for the pin in the LCD_CAM output.
+static uint32_t _PM_directBitMask(Protomatter_core *core, int pin) {
+  if (pin == core->clockPin) return 1 << 6;
+  for (uint8_t i=0; i<6; i++) {
+    if (pin == core->rgbPins[i]) return 1 << i;
+  }
+  // Else return the bit that would normally be used for regular GPIO
+  return (1U << (pin & 31));
+}
+
+// Thankfully, at present, any code which calls _PM_portBitMask() currently
+// has a 'core' variable, so we can do this...
+#define _PM_portBitMask(pin) _PM_directBitMask(core, pin)
+
+// DMA buffer, one row (64 columns x 00RGBRGB)
+// Probably doesn't need alignment, but trying anyway
+static dma_descriptor_t desc;
+static gdma_channel_handle_t dma_chan;
+static volatile bool xfer_done = true; // DMA completion flag
+
+// DMA callback for end-of-transfer interrupt
+static IRAM_ATTR bool dma_callback(gdma_channel_handle_t dma_chan,
+  gdma_event_data_t *event_data, void *user_data) {
+  xfer_done = true;
+  return true;
+}
+
+// LCD_CAM requires a complete replacement of the "blast" functions in order
+// to use the DMA-based peripheral.
+#define _PM_CUSTOM_BLAST // Disable blast_*() functions in core.c
+IRAM_ATTR static void blast_byte(Protomatter_core *core, uint8_t *data) {
+  while (!xfer_done); // Wait for completion of prior xfer
+
+  LCD_CAM.lcd_misc.lcd_afifo_reset = 1; // Reset TX FIFO (required)
+
+  // Re-init parts of descriptor each time (required)
+  desc.dw0.size = desc.dw0.length = core->chainBits;
+  desc.buffer = data;
+
+  xfer_done = false;
+  gdma_start(dma_chan, (intptr_t)&desc);
+  esp_rom_delay_us(1); // Necessary? Another ESP32 example suggests so
+
+  // This begins the DMA xfer out the LCD periph...
+  LCD_CAM.lcd_user.lcd_start = 1;
+}
+
+// If using custom "blast" function(s), all three must be declared.
+// Unused ones can be empty, that's fine, just need to exist.
+IRAM_ATTR static void blast_word(Protomatter_core *core, uint16_t *data) {}
+IRAM_ATTR static void blast_long(Protomatter_core *core, uint32_t *data) {}
+
+//
+// ***** END S3 *** END S3 *** END S3 *** END S3 *** END S3 *** END S3 *****
+#elif defined(CONFIG_IDF_TARGET_ESP32C3) // ********************************
+// *** C3 *** C3 *** C3 *** C3 *** C3 *** C3 *** C3 *** C3 *** C3 *** C3 ***
+//
+
+#define _PM_portOutRegister(pin) (volatile uint32_t *)&GPIO.out
+#define _PM_portSetRegister(pin) (volatile uint32_t *)&GPIO.out_w1ts
+#define _PM_portClearRegister(pin) (volatile uint32_t *)&GPIO.out_w1tc
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
 #define _PM_byteOffset(pin) ((pin & 31) / 8)
@@ -152,6 +240,31 @@ IRAM_ATTR static void blast_long(Protomatter_core *core, uint32_t *data) {}
 #define _PM_byteOffset(pin) (3 - ((pin & 31) / 8))
 #define _PM_wordOffset(pin) (1 - ((pin & 31) / 16))
 #endif
+
+//
+// ***** END C3 *** END C3 *** END C3 *** END C3 *** END C3 *** END C3 *****
+#else // *******************************************************************
+// *** OG *** OG *** OG *** OG *** OG *** OG *** OG *** OG *** OG *** OG ***
+//
+
+#define _PM_portOutRegister(pin)                                               \
+  (volatile uint32_t *)((pin < 32) ? &GPIO.out : &GPIO.out1.val)
+#define _PM_portSetRegister(pin)                                               \
+  (volatile uint32_t *)((pin < 32) ? &GPIO.out_w1ts : &GPIO.out1_w1ts.val)
+#define _PM_portClearRegister(pin)                                             \
+  (volatile uint32_t *)((pin < 32) ? &GPIO.out_w1tc : &GPIO.out1_w1tc.val)
+
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define _PM_byteOffset(pin) ((pin & 31) / 8)
+#define _PM_wordOffset(pin) ((pin & 31) / 16)
+#else
+#define _PM_byteOffset(pin) (3 - ((pin & 31) / 8))
+#define _PM_wordOffset(pin) (1 - ((pin & 31) / 16))
+#endif
+
+//
+// ***** END OG *** END OG *** END OG *** END OG *** END OG *** END OG *****
+#endif // End S2/S3/C3/OG **************************************************
 
 // As written, because it's tied to a specific timer right now, the
 // Arduino lib only permits one instance of the Protomatter_core struct,
@@ -199,9 +312,16 @@ IRAM_ATTR static void _PM_esp32timerCallback(void) {
   _PM_row_handler(_PM_protoPtr); // In core.c
 }
 
+static void pinmux(int8_t pin, uint8_t signal) {
+  esp_rom_gpio_connect_out_signal(pin, signal, false, false);
+  gpio_hal_iomux_func_sel(GPIO_PIN_MUX_REG[pin], PIN_FUNC_GPIO);
+  gpio_set_drive_capability((gpio_num_t)pin, (gpio_drive_cap_t)3);
+}
+
 // Initialize, but do not start, timer (and any other peripherals).
 void _PM_timerInit(Protomatter_core *core) {
 #if defined(CONFIG_IDF_TARGET_ESP32S2)
+
   // On S2, initialize the Dedicated GPIO peripheral using the RGB pin list
   // list from the core struct, plus the clock pin (7 pins total). Unsure if
   // these structs & arrays need to be persistent. Declaring static just in
@@ -222,7 +342,95 @@ void _PM_timerInit(Protomatter_core *core) {
   (void)dedic_gpio_new_bundle(&config_in, &bundle);
   dedic_gpio_bundle_write(bundle, config_in.array_size, 1);
   DEDIC_GPIO.gpio_out_cpu.val = 0; // Use GPIO registers, not CPU instructions
-#endif
+
+#elif defined(CONFIG_IDF_TARGET_ESP32S3)
+
+  // On S3, initialize the LCD_CAM peripheral and DMA.
+
+  // LCD_CAM isn't enabled by default -- MUST begin with this:
+  periph_module_enable(PERIPH_LCD_CAM_MODULE);
+  periph_module_reset(PERIPH_LCD_CAM_MODULE);
+
+  // Configure LCD clock
+  LCD_CAM.lcd_clock.clk_en = 1;             // Enable clock
+  LCD_CAM.lcd_clock.lcd_clk_sel = 3;        // PLL160M source
+  LCD_CAM.lcd_clock.lcd_clkm_div_a = 1;     // 1/1 fractional divide,
+  LCD_CAM.lcd_clock.lcd_clkm_div_b = 1;     // plus '7' below yields...
+  LCD_CAM.lcd_clock.lcd_clkm_div_num = 7;   // 1:8 prescale (20 MHz CLK)
+  LCD_CAM.lcd_clock.lcd_ck_out_edge = 0;    // PCLK low in first half of cycle
+  LCD_CAM.lcd_clock.lcd_ck_idle_edge = 0;   // PCLK low idle
+  LCD_CAM.lcd_clock.lcd_clk_equ_sysclk = 1; // PCLK = CLK (ignore CLKCNT_N)
+
+  // Configure signal pins
+  // IN THEORY this could be expanded to support 2 parallel chains,
+  // but the rest of the LCD & DMA setup isn't currently written for
+  // that, so it's limited to a single chain.
+  const uint8_t signal[] = {
+    LCD_DATA_OUT0_IDX,
+    LCD_DATA_OUT1_IDX,
+    LCD_DATA_OUT2_IDX,
+    LCD_DATA_OUT3_IDX,
+    LCD_DATA_OUT4_IDX,
+    LCD_DATA_OUT5_IDX };
+  for (int i = 0; i < 6; i++) pinmux(core->rgbPins[i], signal[i]);
+  pinmux(core->clockPin, LCD_PCLK_IDX);
+
+  // Configure frame format
+  LCD_CAM.lcd_ctrl.lcd_rgb_mode_en = 0;    // i8080 mode (not RGB)
+  LCD_CAM.lcd_rgb_yuv.lcd_conv_bypass = 0; // Disable RGB/YUV converter
+  LCD_CAM.lcd_misc.lcd_next_frame_en = 0;  // Do NOT auto-frame
+  LCD_CAM.lcd_data_dout_mode.val = 0;      // No data delays
+  LCD_CAM.lcd_user.lcd_dout_cyclelen = core->chainBits;
+  LCD_CAM.lcd_user.lcd_always_out_en = 0;
+  LCD_CAM.lcd_user.lcd_8bits_order = 0; // Do not swap bytes
+  LCD_CAM.lcd_user.lcd_bit_order = 0;   // Do not reverse bit order
+  LCD_CAM.lcd_user.lcd_2byte_en = 0;    // 8-bit data mode
+  LCD_CAM.lcd_user.lcd_dout = 1;        // Enable data out
+  LCD_CAM.lcd_user.lcd_dummy = 0;       // No dummy phase at LCD start
+  LCD_CAM.lcd_user.lcd_cmd = 0;         // No command at LCD start
+  // For whatever reason, a dummy cycle is output at the start
+  // regardless of the 'dummy' setting above. This minimizes it
+  // to one cycle. Since the RGB matrix is simply a shift register,
+  // and this is at the start, it harmlessly goes off the end.
+  // But if there's a way to eliminate this, that would be best.
+  LCD_CAM.lcd_user.lcd_dummy_cyclelen = 0;
+  LCD_CAM.lcd_user.lcd_cmd_2_cycle_en = 0;
+
+  LCD_CAM.lcd_user.lcd_update = 1; // Update registers before 1st xfer
+
+  // Set up DMA TX descriptor
+  desc.dw0.owner = DMA_DESCRIPTOR_BUFFER_OWNER_DMA;
+  desc.dw0.suc_eof = 1;
+  // Size, length and buffer are set on each DMA setup
+  desc.next = NULL;
+
+  // Alloc DMA channel & connect it to LCD periph
+  gdma_channel_alloc_config_t dma_chan_config = {
+    .direction = GDMA_CHANNEL_DIRECTION_TX,
+  };
+  esp_err_t ret = gdma_new_channel(&dma_chan_config, &dma_chan);
+  gdma_connect(dma_chan, GDMA_MAKE_TRIGGER(GDMA_TRIG_PERIPH_LCD, 0));
+  gdma_strategy_config_t strategy_config = {
+    .owner_check = false,
+    .auto_update_desc = false
+  };
+  gdma_apply_strategy(dma_chan, &strategy_config);
+  gdma_transfer_ability_t ability = {
+    .sram_trans_align = 0,
+    .psram_trans_align = 0,
+  };
+  gdma_set_transfer_ability(dma_chan, &ability);
+  gdma_start(dma_chan, (intptr_t)&desc);
+
+  // Enable DMA transfer callback
+  gdma_tx_event_callbacks_t tx_cbs = {
+    .on_trans_eof = dma_callback
+  };
+  gdma_register_tx_event_callbacks(dma_chan, &tx_cbs, NULL);
+
+#endif // end ESP32S2/S3
+
+  // Timer stuff is the same for all ESP32 variants...
 
   hw_timer_t **timer = (hw_timer_t **)core->timer; // pointer-to-pointer
   if (timer == _PM_TIMER_DEFAULT) {
